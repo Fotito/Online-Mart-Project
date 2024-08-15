91237620cfd19bee57e2a66e5745135a9b3d0065
ibsnichyt <109224552+Fotito@users.noreply.github.com>
2024-08-15 22:51:37 +0200

    Update HomeController.cs

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,18 +1,25 @@
+    using itehaProject.Data;
 using itehaProject.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
 
 namespace itehaProject.Controllers
 {
     public class HomeController : Controller
     {
         private  ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
+        private readonly itehaProjectContext _context;
+        public HomeController(ILogger<HomeController> logger, itehaProjectContext context)
         {
             _logger = logger;
+            _context = context;
         }
-
         public IActionResult Index()
         {
             return View();
@@ -23,10 +30,21 @@ namespace itehaProject.Controllers
             return View();
         }
 
-        public IActionResult ProductList()
+        public async Task<IActionResult> ProductList(string id)
         {
-            Products products = new Products();
-            return View();
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var products = await _context.Products
+                .FirstOrDefaultAsync(m => m.ProductID == id);
+            if (products == null)
+            {
+                return NotFound();
+            }
+
+            return View(products);
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
