7b99c71409258aef78107b70e163c63029e7bfca
ibsnichyt <109224552+Fotito@users.noreply.github.com>
2024-08-30 00:45:52 +0200

    added quality attribute. and added getters and setters

--- Models/orderItems.cs
+++ Models/orderItems.cs
@@ -2,13 +2,14 @@
 {
     public class orderItems
     {
-        private string orderId;
-        private string productID;
-
-        public orderItems(string orderId, string productID)
+        private string orderId{get; set;}
+        private string productID{get; set;}
+        private int quantity {get; set;}
+        public orderItems(string orderId, string productID, int quantity)
         {
             this.orderId = orderId;
             this.productID = productID;
+            this.quantity = quantity
         }
     }
 }
