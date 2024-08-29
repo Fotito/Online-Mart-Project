5b0545680e51d42e7b94477465d8ac1e4f315205
ibsnichyt <109224552+Fotito@users.noreply.github.com>
2024-08-29 22:02:55 +0200

    Add files via upload
    added a controller that handles all orders

---
+++ Controllers/OrderController.cs
@@ -0,0 +1,17 @@
+﻿using itehaProject.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Data.Entity;
+
+
+namespace itehaProject.Controllers
+{
+    public class OrderController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
