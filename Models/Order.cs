0a76dfced595ae37d2a1b3bd6bd5560104e7715b
ibsnichyt <109224552+Fotito@users.noreply.github.com>
2024-08-30 12:26:29 +0200

    added field for users

--- Models/Order.cs
+++ Models/Order.cs
@@ -5,12 +5,14 @@
         private string orderID { get; set; }
         private DateOnly dateConfirmed { get; set; }
         private string status { get; set; }
+        private Users Users { get; set; }
 
-        public Order(string orderID, DateOnly dateConfirmed, string status)
+        public Order(string orderID, DateOnly dateConfirmed, string status, Users users)
         {
             this.orderID = orderID;
             this.dateConfirmed = dateConfirmed;
             this.status = status;
+            this.Users = users;
         }
     }
 }
