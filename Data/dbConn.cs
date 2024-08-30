6cf7015e7dcfe205a80d2223001b0f0445e66dc3
ibsnichyt <109224552+Fotito@users.noreply.github.com>
2024-08-30 12:44:10 +0200

    added the namespace so other files can see the dbconn

--- Data/dbConn.cs
+++ Data/dbConn.cs
@@ -1,17 +1,23 @@
 using Microsoft.EntityFrameworkCore;
 using itehaProject.Models;
 
-public class dbConn : DbContext {
-    
-
-    public DbSet<Products> prods { get; set; }
-    public DbSet<Order> orders { get; set; }
-    public DbSet<orderItems> orderItems { get; set; }
-
-    public DbSet<Users> users { get; set; }
-  public dbConn(DbContextOptions<dbConn> options)
-        : base(options) { 
+namespace itehaProject.Data
+{
+    public class dbConn : DbContext
+    {
 
 
-}
+        public DbSet<Products> prods { get; set; }
+        public DbSet<Order> orders { get; set; }
+        public DbSet<orderItems> orderItems { get; set; }
+
+        public DbSet<Users> users { get; set; }
+        public dbConn(DbContextOptions<dbConn> options)
+              : base(options)
+        {
+
+
+        }
+    }
+
 }
